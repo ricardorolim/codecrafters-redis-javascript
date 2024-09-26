84dc69f3e19b96b435cd4a3f818253e089015841
Rickroll <amazingrickroll@gmail.com>
2024-09-27 00:07:29 +0100

    Respond to PING

--- app/main.js
+++ app/main.js
@@ -1,7 +1,10 @@
 const net = require("net");
 
 const server = net.createServer((connection) => {
-  // Handle connection
+    console.log("connected");
+    let response = "+PONG\r\n";
+    connection.write(response);
+    connection.end();
 });
 
 server.listen(6379, "127.0.0.1");
