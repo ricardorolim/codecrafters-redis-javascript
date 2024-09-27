7eed0a04a26be108453cefd07b65d0212fe64a75
Rickroll <amazingrickroll@gmail.com>
2024-09-27 12:03:16 +0100

    Respond to multiple PINGs

--- app/main.js
+++ app/main.js
@@ -1,10 +1,10 @@
 const net = require("net");
 
 const server = net.createServer((connection) => {
-    console.log("connected");
-    let response = "+PONG\r\n";
-    connection.write(response);
-    connection.end();
+    connection.on("data", () => {
+        let response = "+PONG\r\n";
+        connection.write(response);
+    });
 });
 
 server.listen(6379, "127.0.0.1");
