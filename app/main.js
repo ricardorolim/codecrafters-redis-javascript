daa55db4ebf2c80b91bc9217f54fd7fe2d4b075d
Rickroll <amazingrickroll@gmail.com>
2024-09-28 20:22:10 +0100

    Implement the ECHO command

--- app/main.js
+++ app/main.js
@@ -1,10 +1,21 @@
 const net = require("net");
+const redis = require("./redis");
 
-const server = net.createServer((connection) => {
-    connection.on("data", () => {
-        let response = "+PONG\r\n";
-        connection.write(response);
-    });
+async function* asyncStreamByteIterator(stream) {
+    for await (const chunk of stream) {
+        for (const byte of chunk) {
+            yield String.fromCharCode(byte);
+        }
+    }
+}
+
+const server = net.createServer(async (socket) => {
+    let it = asyncStreamByteIterator(socket);
+    const parser = new redis.Parser(it);
+    for await (const command of parser.parseCommand()) {
+        let response = redis.process(command);
+        socket.write(response);
+    }
 });
 
 server.listen(6379, "127.0.0.1");
